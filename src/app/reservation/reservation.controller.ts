d96885a8073f3dcc66c84d27f0962e89762db5bc
Marsboy02 <marsboy0619@gmail.com>
2023-01-11 23:18:22 +0900

    feat: reservation api 의존성 주입

--- src/app/reservation/reservation.controller.ts
+++ src/app/reservation/reservation.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ReservationService } from './reservation.service';
 
 @Controller('reservation')
 @ApiTags('Reservation')
 export class ReservationController {
-  constructor(private readonly reservationController: ReservationController) {}
+  constructor(private readonly reservationServioce: ReservationService) {}
 }
