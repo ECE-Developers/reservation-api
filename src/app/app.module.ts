46090b6226106ad7b7ff45465b1ccf8e23940c24
Marsboy02 <marsboy0619@gmail.com>
2023-01-12 10:03:06 +0900

    delete: nest 기본 api 삭제

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
   imports: [UserModule, ReservationModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
