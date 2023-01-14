bfe691459ff4d52fd12739232e7a88710365e6de
Marsboy02 <marsboy0619@gmail.com>
2023-01-14 16:09:26 +0900

    feat: swagger에 상태 코드 type 추가

--- src/app/reservation/reservation.controller.ts
+++ src/app/reservation/reservation.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ReservationService } from './reservation.service';
+import { OkSuccess } from '../../libs/response/status-code/ok.success';
+import { CreatedSuccess } from '../../libs/response/status-code/created.success';
 
 @Controller('reservation')
 @ApiTags('Reservation')
@@ -11,6 +13,7 @@ export class ReservationController {
   @ApiResponse({
     status: 200,
     description: '모든 reservation입니다.',
+    type: OkSuccess,
   })
   @ApiOperation({ summary: 'reservation을 모두 조회합니다.' })
   getReservations() {
@@ -21,6 +24,7 @@ export class ReservationController {
   @ApiResponse({
     status: 200,
     description: 'id로 조회한 reservation입니다.',
+    type: OkSuccess,
   })
   @ApiOperation({ summary: 'reservation을 하나 조회합니다.' })
   getReservationOne(@Param() id: number) {
@@ -31,6 +35,7 @@ export class ReservationController {
   @ApiResponse({
     status: 201,
     description: 'reservation이 생성되었습니다.',
+    type: CreatedSuccess,
   })
   @ApiOperation({ summary: 'reservation을 생성합니다.' })
   createReservation(@Param() id: number) {
@@ -41,6 +46,7 @@ export class ReservationController {
   @ApiResponse({
     status: 200,
     description: 'reservation이 삭제되었습니다.',
+    type: OkSuccess,
   })
   @ApiOperation({ summary: 'reservation을 삭제합니다.' })
   deleteReservation(@Param() id: number) {
--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
+import { CreatedSuccess } from '../../libs/response/status-code/created.success';
+import { OkSuccess } from '../../libs/response/status-code/ok.success';
 
 @Controller('user')
 @ApiTags('User')
@@ -21,6 +23,7 @@ export class UserController {
   @ApiResponse({
     status: 201,
     description: 'user가 생성되었습니다.',
+    type: CreatedSuccess,
   })
   @ApiOperation({ summary: 'user의 정보를 생성합니다.' })
   createUser(@Param() id: number) {
@@ -31,6 +34,7 @@ export class UserController {
   @ApiResponse({
     status: 200,
     description: 'user가 삭제됐습니다.',
+    type: OkSuccess,
   })
   @ApiOperation({ summary: 'user의 정보를 삭제합니다.' })
   deleteUser(@Param() id: number) {
@@ -41,6 +45,7 @@ export class UserController {
   @ApiResponse({
     status: 200,
     description: 'user의 정보가 갱신되었습니다.',
+    type: OkSuccess,
   })
   @ApiOperation({ summary: 'user의 고유 번호를 교체합니다.' })
   patchUser(@Param() id: number) {
