1e59921c564588920e6d4f79bb33d581e0d4ce7a
Marsboy02 <marsboy0619@gmail.com>
2023-01-27 18:29:37 +0900

    feat: auth api 스웨거 세팅 추가

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreatedSuccess } from '../../libs/response/status-code/created.success';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { OkSuccess } from '../../libs/response/status-code/ok.success';
 
 @Controller('auth')
 @ApiTags('Auth')
@@ -14,8 +21,20 @@ export class AuthController {
     description: 'jwt 토큰이 발급되었습니다.',
     type: CreatedSuccess,
   })
+  @ApiOperation({ summary: 'username과 password를 통해 로그인합니다.' })
   async login(@Req() request) {
-    console.log(request.body);
     return this.authService.login(request.body);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  @ApiOkResponse({
+    status: 200,
+    description: 'jwt 토큰을 반환합니다.',
+    type: OkSuccess,
+  })
+  @ApiOperation({ summary: 'jwt 토큰을 확인합니다.' })
+  getProfile(@Req() req) {
+    return req.user;
   }
 }
