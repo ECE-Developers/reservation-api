e2003acbe9c179d8ee28160cea689a330e9eab36
Marsboy02 <marsboy0619@gmail.com>
2023-01-29 15:54:20 +0900

    fix: api가 돌아가는 url 설정

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,9 @@ async function bootstrap() {
 
   app.enableCors({ origin: true, credentials: true });
 
-  await app.listen(process.env.APP_PORT || 3000, '0.0.0.0');
+  await app.listen(
+    process.env.APP_PORT || 3000,
+    process.env.APP_URL || 'localhost',
+  );
 }
 bootstrap();
