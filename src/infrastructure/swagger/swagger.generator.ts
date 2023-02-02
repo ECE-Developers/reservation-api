cb162b5c258fe057018fed3e9451351f6dcfeba3
Marsboy02 <marsboy0619@gmail.com>
2023-02-02 15:38:59 +0900

    feat: swagger에 jwt 세팅 추가

--- src/infrastructure/swagger/swagger.generator.ts
+++ src/infrastructure/swagger/swagger.generator.ts
@@ -1,12 +1,22 @@
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as process from 'process';
 import { tags } from './swagger.tags';
 
 const document = new DocumentBuilder()
   .setTitle(`Reservation API`)
   .setDescription(`Reservation API docs`)
   .setContact('ECE-Developers', 'www.ece.kr', 'marsboy0619@gmail.com')
-  .setVersion('0.0.1');
+  .setVersion('0.0.1')
+  .addBearerAuth(
+    {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'JWT',
+      description: 'Enter JWT token',
+      in: 'header',
+    },
+    'access_token',
+  );
 
 tags.forEach((tag) => document.addTag(tag.name, tag.description));
 
