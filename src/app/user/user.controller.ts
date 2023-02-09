0a213db2a8230afe4541278f85ebb1ff7ec2c24a
Marsboy02 <marsboy0619@gmail.com>
2023-02-09 16:23:09 +0900

    feat: users api에 jwt guard 추가

--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { UserService } from './user.service';
 import {
   ApiBadRequestResponse,
+  ApiBearerAuth,
   ApiCreatedResponse,
   ApiInternalServerErrorResponse,
   ApiNotFoundResponse,
@@ -17,6 +18,7 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { deleteUserRequest } from '../../libs/request/users/delete-user.request';
 import { CreateUserRequest } from '../../libs/request/users/create-user.request';
@@ -30,6 +32,7 @@ import { UserIdRequest } from '../../libs/request/users/user-id.request';
 import { ReadUserSuccessResponse } from '../../libs/response/users/read-user.success.response';
 import { UpdateUserSuccessResponse } from '../../libs/response/users/update-user.success.response';
 import { DeleteUserSuccessResponse } from '../../libs/response/users/delete-user.success.response';
+import { JwtAuthGuard } from '../auth/jwt/jwt-auth.guard';
 
 @Controller('users')
 @ApiTags('User')
@@ -40,14 +43,6 @@ export class UserController {
   @ApiOkResponse({
     description: 'user를 성공적으로 조회한 경우',
     type: ReadUserSuccessResponse,
-  })
-  @ApiBadRequestResponse({
-    description: '클라이언트에서 잘못된 요청을 보낸 경우',
-    type: BadRequestError,
-  })
-  @ApiUnauthorizedResponse({
-    description: '인증이 안된 경우',
-    type: UnauthorizedError,
   })
   @ApiNotFoundResponse({
     description: '리소스가 없는 경우',
@@ -66,12 +61,8 @@ export class UserController {
     description: '회원 가입에 성공한 경우',
     type: CreateUserSuccessResponse,
   })
-  @ApiBadRequestResponse({
-    description: '클라이언트에서 잘못된 요청을 보낸 경우',
-    type: BadRequestError,
-  })
   @ApiInternalServerErrorResponse({
-    description: '서버 에러가 발생한 경우',
+    description: '회원 가입에 실패한 경우',
     type: InternalServerErrorError,
   })
   @ApiOperation({ summary: '계정 생성' })
@@ -80,6 +71,8 @@ export class UserController {
   }
 
   @Patch()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access_token')
   @ApiOkResponse({
     description: 'password가 성공적으로 변경되었습니다.',
     type: UpdateUserSuccessResponse,
@@ -105,13 +98,11 @@ export class UserController {
     return this.userService.updateUser(body);
   }
   @Delete()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access_token')
   @ApiOkResponse({
     description: '계정 삭제에 성공한 경우',
     type: DeleteUserSuccessResponse,
-  })
-  @ApiBadRequestResponse({
-    description: '클라이언트에서 잘못된 요청을 보낸 경우',
-    type: BadRequestError,
   })
   @ApiUnauthorizedResponse({
     description: '인증이 안된 경우',
