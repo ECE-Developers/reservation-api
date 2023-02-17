f4534d8403aa2c32fed98801a41f55f852a2c603
Marsboy02 <marsboy0619@gmail.com>
2023-02-17 10:45:18 +0900

    feat: refresh token 관련 api 추가

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -11,6 +11,7 @@ import { UsernameRequest } from '../../libs/request/users/username.request';
 import { UserEntity } from '../../libs/entity/user.entity';
 import { LoginRequest } from '../../libs/request/auth/login.request';
 import * as argon2 from 'argon2';
+import * as process from 'process';
 
 @Injectable()
 export class AuthService {
@@ -40,7 +41,7 @@ export class AuthService {
 
   async checkUsername(dto: UsernameRequest): Promise<object> {
     try {
-      const user = await this.authRepository.findOne(dto.username);
+      const user = await this.authRepository.getUserByUsername(dto.username);
       if (!user) throw new NotFoundException('존재하지 않는 username입니다.');
       return {
         id: user.id,
@@ -58,9 +59,81 @@ export class AuthService {
     username: string,
     pass: string,
   ): Promise<UserEntity> {
-    const user = await this.authRepository.findOne(username);
+    const user = await this.authRepository.getUserByUsername(username);
     if (!user) throw new NotFoundException('존재하지 않는 username입니다.');
     if (await argon2.verify(user.password, pass)) return user;
     else throw new BadRequestException('password가 일치하지 않습니다.');
+  }
+
+  getCookieWithJwtAccessToken(id: number) {
+    const payload = { id };
+    const token = this.jwtService.sign(payload, {
+      secret: process.env.JWT_ACCESS_TOKEN_SECRET,
+      expiresIn: `${process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME}s`,
+    });
+
+    return {
+      accessToken: token,
+      domain: 'localhost',
+      path: '/',
+      httpOnly: true,
+      maxAge: Number(process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME) * 1000,
+    };
+  }
+
+  getCookieWithJwtRefreshToken(id: number) {
+    const payload = { id };
+    const token = this.jwtService.sign(payload, {
+      secret: process.env.JWT_REFRESH_TOKEN_SECRET,
+      expiresIn: `${process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME}s`,
+    });
+
+    return {
+      refreshToken: token,
+      domain: 'localhost',
+      path: '/',
+      httpOnly: true,
+      maxAge: Number(process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME) * 1000,
+    };
+  }
+
+  getCookiesForLogOut() {
+    return {
+      accessOption: {
+        domain: 'localhost',
+        path: '/',
+        httpOnly: true,
+        maxAge: 0,
+      },
+      refreshOption: {
+        domain: 'localhost',
+        path: '/',
+        httpOnly: true,
+        maxAge: 0,
+      },
+    };
+  }
+
+  async setCurrentRefreshToken(refreshToken: string, id: number) {
+    const currentHashedRefreshToken = await argon2.hash(refreshToken);
+    await this.authRepository.updateRefreshToken(id, {
+      currentHashedRefreshToken,
+    });
+  }
+
+  async getUserIfRefreshTokenMatches(refreshToken: string, id: number) {
+    const user = await this.authRepository.getUserById(id);
+    const isRefreshTokenMatching = await argon2.verify(
+      refreshToken,
+      user.currentHashedRefreshToken,
+    );
+
+    if (isRefreshTokenMatching) return user;
+  }
+
+  async removeRefreshToken(id: number) {
+    return this.authRepository.updateRefreshToken(id, {
+      currentHashedRefreshToken: null,
+    });
   }
 }
