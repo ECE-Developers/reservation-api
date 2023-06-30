4f46d3d5885949979ad2bef480e0d24561cdb0ef
Marsboy02 <marsboy0619@gmail.com>
2023-06-30 21:07:54 +0900

    chore: swagger 버전 업그레이드를 통해 의존성 에러 해결

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,43 +2,8 @@ import { Module } from '@nestjs/common';
 import { ReservationModule } from './reservation/reservation.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { AdminModule } from '@adminjs/nestjs';
-
-const DEFAULT_ADMIN = {
-  email: 'admin@example.com',
-  password: 'password',
-};
-
-const authenticate = async (email: string, password: string) => {
-  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
-    return Promise.resolve(DEFAULT_ADMIN);
-  }
-  return null;
-};
 
 @Module({
-  imports: [
-    ReservationModule,
-    UserModule,
-    AuthModule,
-    AdminModule.createAdminAsync({
-      useFactory: () => ({
-        adminJsOptions: {
-          rootPath: '/admin',
-          resources: [],
-        },
-        auth: {
-          authenticate,
-          cookieName: 'adminjs',
-          cookiePassword: 'secret',
-        },
-        sessionOptions: {
-          resave: true,
-          saveUninitialized: true,
-          secret: 'secret',
-        },
-      }),
-    }),
-  ],
+  imports: [ReservationModule, UserModule, AuthModule],
 })
 export class AppModule {}
